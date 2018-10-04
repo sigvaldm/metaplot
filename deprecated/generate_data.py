c5e2476e20381235964528da6df511f71f0d9b9e
sigvaldm <sigvaldm@fys.uio.no>
2018-10-04 20:54:45 +0200

    Deleted deprecated files

--- deprecated/generate_data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-xs = np.linspace(0, 2*np.pi, 100)
-ys1 = np.cos(xs)
-ys2 = np.sin(xs)
-
-with open('dummy.txt','w') as f:
-    f.write('#SYMB t\tV\tI\n')
-    for x,y1,y2 in zip(xs,ys1,ys2):
-        f.write("{}\t{}\t{}\n".format(x,y1,y2))
